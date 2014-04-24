796a5ebc4f0d6d7d3dafea652cebee6d02ec8210
jslopeza <jasonlopeza95@gmail.com>
2014-04-24 15:41:21 -0400

    passed jquery to view, product views are now working

--- js/collections/products.js
+++ js/collections/products.js
@@ -9,15 +9,15 @@
 			name: 'Product01',
 			price: '$ ' + 2.50,
 		},
-			{
+		{
 			name: 'Product02',
 			price: '$ ' + 3.50,
 		},
-			{
+		{
 			name: 'Product03',
 			price: '$ ' + 4.50,
 		},
-			{
+		{
 			name: 'Product04',
 			price: '$ ' + 5.50,
 		},
