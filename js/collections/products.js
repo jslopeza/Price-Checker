fc67269ae28fab91c6587951bd3755f178ef2508
jslopeza <jasonlopeza95@gmail.com>
2014-04-26 15:01:35 -0400

    Removed product collection

--- js/collections/products.js
+++ js/collections/products.js
@@ -7,31 +7,4 @@
 	
 	window.products = new POS.Collections.Products;
 	products.fetch();
-	/*window.products = new POS.Collections.Products ([
-		{
-			name: 'Product01',
-			price: '$ ' + 2.50,
-		},
-		{
-			name: 'Product02',
-			price: '$ ' + 3.50,
-		},
-		{
-			name: 'Product03',
-			price: '$ ' + 4.50,
-		},
-		{
-			name: 'Product04',
-			price: '$ ' + 5.50,
-		},
-		{
-			name: 'Product05',
-			price: '$ ' + 7.50,
-		},
-		{
-			name: 'Product05',
-			price: '$ ' + 1.50,
-		}
-
-	]);*/
 })();
