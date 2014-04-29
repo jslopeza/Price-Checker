11f545543a3cd1cff06535d2e476962bcbc4b295
jslopeza <jasonlopeza95@gmail.com>
2014-04-29 14:40:37 -0400

    Get product name on clicking the product

--- js/views/checkout.js
+++ js/views/checkout.js
@@ -1,28 +1,21 @@
 (function($){
 
 	POS.Views.Checkout = Backbone.View.extend({
-		el : '.checkout',
-
-		//tagName : 'td',
-
-		template : "checkoutView",
-
-		events : {
-			'click .checkout-td' : 'getProduct'
-		},
-		render : function(){
-			return this;
-		},
-
-		showAlert : function(){
-			alert('click working');
-		},
-
-		getProduct : function(){
-			var id = $('.checkout-td').map(function(){
-				return $(this).text();
-			});
-			console.log(id);
+		el : 'td',
+ 
+ 		events : {
+			'click a' : 'getProduct'
+ 		},
+ 
+ 		showAlert : function(){
+ 			alert('Working from a different View');
+ 		},
+		getProduct : function(e){
+			e.preventDefault();
+			var id = $(e.currentTarget).data('id');
+			var item = this.collection.at(id);
+			var product = item.get('name')
+			console.log(product);
 		}
 
 	});
