f669b61c4b6317f89b9f97f077ceb813bf740ecc
jslopeza <jasonlopeza95@gmail.com>
2014-05-03 13:04:09 -0400

    Rendering on click

--- js/views/checkout.js
+++ js/views/checkout.js
@@ -1,5 +1,14 @@
 (function($){
 
+	POS.Views.Checkouts = Backbone.View.extend({
+		tagName : 'tr',
+ 
+ 		render : function(checkoutModel){
+ 			var checkoutCollection = new POS.Collections.Checkout({model : checkoutModel});
+ 			console.log(checkoutCollection);
+ 		}
+ 	});
+
 	POS.Views.Checkout = Backbone.View.extend({
  		
  		el : '.checkout tbody',
@@ -8,13 +17,12 @@
  		template : template('checkoutTemplate'),
  
  		initialize: function() {
-			Backbone.Events.on("click", this.sendData);
+			Backbone.Events.on("click", this.sendData, this);
 		},
 		sendData: function(id, name, price) {
 			
 			window.checkoutModel = new POS.Models.Checkout({id : id, name : name, price : price });
 			console.log(checkoutModel.toJSON());
-			return checkoutModel;
 			this.render();
 		},
 	
