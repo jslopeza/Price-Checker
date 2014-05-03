877f78bf6f263f64fc248c519f5ed8861cc1ba0c
jslopeza <jasonlopeza95@gmail.com>
2014-05-03 12:58:09 -0400

    The model gets rendered to the table on console commands

--- js/views/checkout.js
+++ js/views/checkout.js
@@ -2,40 +2,25 @@
 
 	POS.Views.Checkout = Backbone.View.extend({
  		
- 		tagName : 'li',
+ 		el : '.checkout tbody',
+ +
+ + 		//tagName : 'td',
  		template : template('checkoutTemplate'),
  
- 		showAlert : function(){
- 			alert('Working from a different View');
- 		},
  		initialize: function() {
 			Backbone.Events.on("click", this.sendData);
 		},
 		sendData: function(id, name, price) {
 			
-			window.checkoutModel = new POS.Collections.Checkout({id : id, name : name, price : price });
-			console.log(checkoutModel.toJSON());
-			return checkoutModel;
-		},
-		/*getProduct : function(e){
-			e.preventDefault();
-			var location = $(e.currentTarget).data('id');
-			var item = this.collection.at(location);
-			var id = item.get('id');
-			var name = item.get('name');
-			var price = item.get('price');
-
-			console.log(location, id, name, price);
-			window.checkoutModel = new POS.Collections.Checkout({id : id, name : name, price : price });
+			window.checkoutModel = new POS.Models.Checkout({id : id, name : name, price : price });
 			console.log(checkoutModel.toJSON());
 			return checkoutModel;
 			this.render();
-		},*/
-
+		},
+	
 		render : function(){
-			window.template = this.template(checkoutModel.toJSON());
-			console.log(template);
-			//this.$el.html(template));
+			var template = this.template(checkoutModel.toJSON());
+ +			this.setElement(this.$el.html(template));
 			return this;
 		}
 
