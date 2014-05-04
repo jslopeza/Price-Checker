46b85a287578a2bca51c6e6694f4ad7470bf9581
jslopeza <jasonlopeza95@gmail.com>
2014-05-03 23:35:28 -0400

    changed the element to point to the right position

--- js/views/checkout.js
+++ js/views/checkout.js
@@ -1,20 +1,21 @@
 (function($){
 	POS.Views.Checkout = Backbone.View.extend({
  		
- 		el : '.checkout',
- 		
+ 		el : '.checkout table',
+
  		template : template('checkoutTemplate'),
  
  		initialize: function() {
 			Backbone.Events.on("click", this.sendData, this);
 		},
+
 		sendData: function(id, name, subtotal, tax, total) {
 			
 			window.checkoutModel = new POS.Models.Checkout({name : name, subtotal : subtotal, tax : tax, total : total});
 			console.log(checkoutModel.toJSON());
 			this.render();
 		},
-	
+		
 		render : function(){
 			var template = this.template(checkoutModel.toJSON());
  			this.setElement(this.$el.html(template));
