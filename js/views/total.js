21268f6adb87850b3a0e734641a8a751249534eb
jslopeza <jasonlopeza95@gmail.com>
2014-05-03 23:36:03 -0400

    Shows the total of the products when the find total button is clicked

---
+++ js/views/total.js
@@ -0,0 +1,19 @@
+(function($){
+	
+	POS.Views.Total = Backbone.View.extend({
+		el : '.end',
+
+		template : template('checkoutTemplate'),
+
+		events : {
+			'click a' : 'showTotal'
+		},
+
+		showTotal : function(){
+			var total = checkoutModel.get('total');
+			alert(total);
+			navigator.notification.vibrate(2000);
+		},
+	});
+
+})(jQuery);
