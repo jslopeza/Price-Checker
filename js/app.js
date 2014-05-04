39894b89e77dc9499fa1ad31f580f155fde2b774
jslopeza <jasonlopeza95@gmail.com>
2014-05-03 23:34:21 -0400

    removed onclick event, it only expands products and makes instance of the total and checkout view

--- js/app.js
+++ js/app.js
@@ -2,21 +2,6 @@ function showProducts(){
 	$('.products').html(productsView.render().el);
 	$('#productBtn').css('visibility','hidden');
 
-	window.checkout = new POS.Views.Checkout;
-}
-
-/*document.addEventListener("deviceready", onDeviceReady, false);
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-        // Empty
-    }*/
-
-
-// Phonegap vibrate 
-function endSale() {
-	
-	alert(this.model.get('total'));
-	navigator.notification.vibrate(2000);
+	var checkout = new POS.Views.Checkout;
+	var total = new POS.Views.Total;
 }
